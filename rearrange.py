532eb7160d9ef43b104025da1671dd51bbdb5240
Luke <deandrevidal@aol.com>
2020-01-23 11:28:35 -0800

    rearrange.py

---
+++ rearrange.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+words = sys.argv[1:]
+
+shuffled = []
+
+
+def rearrange():
+    for i in range(len(words)):
+        rand_index = random.randint(0, len(words) - 1)
+        shuffled.append(words[rand_index])
+
+
+if __name__ == '__main__':
+    word = rearrange()
+    print(words[0:])
+    print(*shuffled)
